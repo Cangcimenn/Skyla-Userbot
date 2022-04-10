74d83633fd33dd8a17484d0d87a6892191f80b81
sᴋʏ <92210483+Cangcimenn@users.noreply.github.com>
2022-04-10 11:46:31 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -20,7 +20,7 @@ from telethon import events
 from userbot import CMD_HANDLER, DEFAULT, CMD_LIST, DEVS, bot
 
 
-def grovy_cmd(pattern=None, command=None, **args):
+def skyla_cmd(pattern=None, command=None, **args):
     args["func"] = lambda e: e.via_bot_id is None
     stack = inspect.stack()
     previous_stack_frame = stack[1]
