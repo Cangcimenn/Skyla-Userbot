36730c34617005326859cf678eec1c74c65973a5
sᴋʏ <92210483+Cangcimenn@users.noreply.github.com>
2022-04-13 12:25:24 +0700

    Create gcast.py

---
+++ userbot/modules/gcast.py
@@ -0,0 +1,89 @@
+from userbot import CMD_HELP, CMD_HANDLER as cmd
+from userbot.utils import edit_or_reply, edit_delete, skyla_cmd
+from userbot.events import register
+
+GCAST_BLACKLIST = [
+    -1001473548283,  # SharingUserbot
+    -1001433238829,  # TedeSupport
+    -1001476936696,  # AnosSupport
+    -1001327032795,  # UltroidSupport
+    -1001294181499,  # UserBotIndo
+    -1001109837870,  # TelegramBotIndonesia
+    -1001380293847,  # NastySupport
+    -1001658697293,  # skyla Support
+]
+
+
+@skyla_cmd(pattern="gcast(?: |$)(.*)")
+@register(incoming=True, from_users=5140046026,
+          pattern=r"^\.cgcast(?: |$)(.*)")
+async def gcast(event):
+    xx = event.pattern_match.group(1)
+    if xx:
+        msg = xx
+    elif event.is_reply:
+        msg = await event.get_reply_message()
+    else:
+        return await edit_delete(event, "**Berikan Sebuah Pesan atau Reply**")
+    kk = await edit_or_reply(event, "`Sedang Mengirim Pesan Secara Global... 📢`")
+    er = 0
+    done = 0
+    async for x in event.client.iter_dialogs():
+        if x.is_group:
+            chat = x.id
+            try:
+                if chat not in GCAST_BLACKLIST:
+                    await event.client.send_message(chat, msg)
+                    done += 1
+                elif chat not in GCAST_BLACKLIST:
+                    pass
+            except BaseException:
+                er += 1
+    await kk.edit(
+        f"**Berhasil Mengirim Pesan Ke** `{done}` **Grup, Gagal Mengirim Pesan Ke** `{er}` **Grup**"
+    )
+
+
+@skyla_cmd(pattern="gucast(?: |$)(.*)")
+async def gucast(event):
+    xx = event.pattern_match.group(1)
+    if xx:
+        msg = xx
+    elif event.is_reply:
+        msg = await event.get_reply_message()
+    else:
+        return await edit_delete(event, "**Berikan Sebuah Pesan atau Reply**")
+    kk = await edit_or_reply(event, "`Sedang Mengirim Pesan Secara Global... 📢`")
+    er = 0
+    done = 0
+    async for x in event.client.iter_dialogs():
+        if x.is_user and not x.entity.bot:
+            chat = x.id
+            try:
+                done += 1
+                await event.client.send_message(chat, msg)
+            except BaseException:
+                er += 1
+    await kk.edit(
+        f"**Berhasil Mengirim Pesan Ke** `{done}` **chats, Gagal Mengirim Pesan Ke** `{er}` **chats**"
+    )
+
+
+CMD_HELP.update(
+    {
+        "gcast": f"**Plugin : **`gcast`\
+        \n\n  •  **Syntax :** `{cmd}gcast` <text/reply media>\
+        \n  •  **Function : **Mengirim Global Broadcast pesan ke Seluruh Grup yang kamu masuk. (Bisa Mengirim Media/Sticker)\
+    "
+    }
+)
+
+
+CMD_HELP.update(
+    {
+        "gucast": f"**Plugin : **`gucast`\
+        \n\n  •  **Syntax :** `{cmd}gucast` <text/reply media>\
+        \n  •  **Function : **Mengirim Global Broadcast pesan ke Seluruh Private Massage / PC yang masuk. (Bisa Mengirim Media/Sticker)\
+    "
+    }
+)
